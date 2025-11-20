703b6b75d0d1ba25f1fe6664d53b681d59567102
JhonathanDavidGallego <samumati0315@gmail.com>
2025-11-19 19:19:48 -0500

    Clase_6_Agenda_ADSO_v4_Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-// React: la librería principal
-
 import ReactDOM from 'react-dom/client'
-// ReactDOM: dibuja React en el navegador
-
 import App from './App.jsx'
-// Importamos el componente raíz (nuestra Agenda ADSO)
-
-import './index.css'
-// Importamos los estilos globales
+import './index.css'   // <--- IMPORTANTE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* Aquí montamos nuestra Agenda ADSO */}
   </React.StrictMode>,
-);
+)
