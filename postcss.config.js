ffca5d169132836519d96fbf85815a92d8c5db51
JhonathanDavidGallego <samumati0315@gmail.com>
2025-11-26 19:50:49 -0500

    Clase_6_Agenda_ADSO_v4_TailwindSolucion

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+// Configuración de PostCSS para Tailwind v3.4 (estable)
+//export default {
+  //plugins: {
+    // Registra Tailwind como plugin de PostCSS
+   // tailwindcss: {},
+    // Autoprefixer agrega compatibilidad de prefijos CSS
+    //autoprefixer: {},
+  //},
+//};
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
+    // 'autoprefixer': {}, // Esto es opcional en v4
   },
-}
+};
+
